5e1d8fbeeefd95152c5703909c4e82a9dae3e2e1
Adam Fields <adam.elliot.fields@gmail.com>
2017-08-21 19:44:38 -0400

    Replaced for loop with forEach and lambda.

--- java-development-environment/systemizer/src/Systemizer.java
+++ java-development-environment/systemizer/src/Systemizer.java
@@ -3,20 +3,13 @@ import java.util.TreeSet;
 
 public class Systemizer {
   public static void main(String[] args) {
-    System.out.printf(
-        "This is the classpath: %s%n",
-        System.getProperty("java.class.path")
-    );
+    System.out.println("This is the classpath: " + System.getProperty("java.class.path"));
+
     // Because Set is given an explicit type of String, we can pass the diamond operator to TreeSet
     // This is syntactic sugar for Set<String> propNames = new TreeSet<String>...
     Set<String> propNames = new TreeSet<>(System.getProperties().stringPropertyNames());
 
-    for (String propertyName : propNames) {
-      System.out.printf(
-          "%s is %s%n",
-          propertyName,
-          System.getProperty(propertyName)
-      );
-    }
+    propNames
+        .forEach(item -> System.out.println(item + ": " + System.getProperty(item)));
   }
 }
