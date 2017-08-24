4c9a622b74d73921f5652b0414863fe76eb8c47c
Adam Fields <adam.elliot.fields@gmail.com>
2017-08-24 09:04:07 -0400

    Changed Java Strings project settings.

--- java-strings/explore/src/main/java/io/github/adamelliotfields/Main.java
+++ java-strings/explore/src/main/java/io/github/adamelliotfields/Main.java
@@ -1,4 +1,6 @@
-public class Explore {
+package io.github.adamelliotfields;
+
+public class Main {
   public static final String ANSI_RESET = "\u001B[0m";
   public static final String ANSI_RED = "\u001B[31m";
   public static final String ANSI_GREEN = "\u001B[32m";
