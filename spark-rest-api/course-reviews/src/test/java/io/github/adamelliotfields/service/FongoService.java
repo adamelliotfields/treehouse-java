026c369512ff8ed883c56e4205dc03733a60dfdc
Adam Fields <adam.elliot.fields@gmail.com>
2017-09-02 11:03:59 -0400

    Switched JCenter to MavenCentral; switched Hamcrest to AssertJ.

--- spark-rest-api/course-reviews/src/test/java/io/github/adamelliotfields/service/FongoService.java
+++ spark-rest-api/course-reviews/src/test/java/io/github/adamelliotfields/service/FongoService.java
@@ -1,36 +1,39 @@
-package io.github.adamelliotfields.service;
+package io.github.adamelliotfields.service
 
-import com.github.fakemongo.Fongo;
-import com.mongodb.MongoClient;
-import io.github.adamelliotfields.entity.Course;
-import io.github.adamelliotfields.entity.Review;
-import lombok.Getter;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
+import com.github.fakemongo.Fongo
+import com.mongodb.MongoClient
+import io.github.adamelliotfields.entity.Course
+import io.github.adamelliotfields.entity.Review
+import lombok.Getter
+import org.mongodb.morphia.Datastore
+import org.mongodb.morphia.Morphia
 
-public class FongoService {
-  @Getter
-  private static final FongoService instance = new FongoService();
+class FongoService private constructor() {
 
-  @Getter
-  private final Fongo fongo;
+    @Getter
+    val fongo: Fongo
 
-  @Getter
-  private final MongoClient fongoClient;
+    @Getter
+    val fongoClient: MongoClient
 
-  @Getter
-  private final Morphia morphia;
+    @Getter
+    val morphia: Morphia
 
-  @Getter
-  private final Datastore datastore;
+    @Getter
+    val datastore: Datastore
 
-  private FongoService() {
-    fongo = new Fongo("test");
-    morphia = new Morphia();
-    fongoClient = fongo.getMongo();
+    init {
+        fongo = Fongo("test")
+        morphia = Morphia()
+        fongoClient = fongo.mongo
 
-    morphia.map(Course.class, Review.class);
+        morphia.map(Course::class.java, Review::class.java)
 
-    datastore = morphia.createDatastore(fongoClient, "test");
-  }
+        datastore = morphia.createDatastore(fongoClient, "test")
+    }
+
+    companion object {
+        @Getter
+        val instance = FongoService()
+    }
 }
