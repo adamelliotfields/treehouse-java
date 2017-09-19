de6b3082a55815e1df1bf01f26c6bb1bcfe44eba
Adam Fields <adam.elliot.fields@gmail.com>
2017-09-19 18:36:59 -0400

    Increased Tomcat max file size.

--- spring-data/gif-lib/src/main/java/io/github/adamelliotfields/config/WebConfig.java
+++ spring-data/gif-lib/src/main/java/io/github/adamelliotfields/config/WebConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class WebConfig {
   // Support sending up to 10MB through forms
   @Bean
   MultipartConfigElement multipartConfig() {
